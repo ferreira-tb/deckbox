f36d57a598100ae1eaa09c92f8d5a2024832b3cf
Andrew Ferreira <andrew.shien2@gmail.com>
2026-04-23 15:16:41 -0300

    feat: show min card price (#20)

--- app/src/components/ygo-card/YgoCardGridSide.vue
+++ app/src/components/ygo-card/YgoCardGridSide.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { computed, type VNode } from 'vue';
+import type { Option } from '@tb-dev/utils';
+import { Badge } from '@tb-dev/vue-components';
 import type { Db_CardId } from '@/lib/bindings';
 import type { CardImpl } from '@/lib/model/card';
 import { useAmountInTrunk } from '@/composables/useAmountInTrunk';
@@ -11,6 +13,7 @@ interface Props {
 interface ActionSlotProps {
   cardId: Db_CardId;
   inTrunk: number;
+  price: Option<number>;
 }
 
 const props = defineProps<Props>();
@@ -21,6 +24,17 @@ defineSlots<{
 
 const cardId = computed(() => props.card.cardId);
 const inTrunk = useAmountInTrunk(cardId);
+
+const price = computed(() => {
+  if (props.card.price) {
+    const value = Number.parseFloat(props.card.price);
+    if (Number.isFinite(value) && value > 0) {
+      return value;
+    }
+  }
+
+  return null;
+});
 
 const description = computed(() => {
   return props.card.description
@@ -34,15 +48,21 @@ const description = computed(() => {
   <div class="h-full flex flex-col gap-2 p-2">
     <img :src="card.imagePath" :alt="card.name" loading="eager" class="max-h-2/4 object-scale-down" />
     <div class="h-full flex flex-col justify-between gap-1 overflow-hidden">
-      <div class="flex flex-col overflow-x-hidden overflow-y-auto pr-2">
-        <h1 class="font-bold">{{ card.name }}</h1>
+      <div class="flex flex-col gap-1 overflow-x-hidden overflow-y-auto pr-2">
+        <div class="flex justify-between gap-2">
+          <h1 class="font-bold">{{ card.name }}</h1>
+          <Badge v-if="price" variant="outline" class="max-h-max">
+            <span>{{ `$${price.toFixed(2)}` }}</span>
+          </Badge>
+        </div>
+
         <p v-for="(line, idx) of description" :key="idx">{{ line }}</p>
       </div>
 
       <slot
         v-if="$slots.action"
         name="action"
-        v-bind="{ cardId, inTrunk }"
+        v-bind="{ cardId, inTrunk, price }"
       ></slot>
     </div>
   </div>
